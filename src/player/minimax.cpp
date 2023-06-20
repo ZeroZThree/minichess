4b9f9d342537afd03b87361be8dace8a966967d4
ZeroZThree <1024621agar@gmail.com>
2023-06-20 16:16:59 +0800

    evaluate version 2

--- src/player/minimax.cpp
+++ src/player/minimax.cpp
@@ -51,7 +51,7 @@ void write_valid_spot(std::ofstream& fout) {
     depth++;
     // Remember to flush the output to ensure the last action is written to file.
     fout.flush();
-    break;
+    //break;
   }
 }
 
